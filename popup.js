b8c231ee2a4dbcf002bffcbfc0995b138521d2dc
chandlerlei2017 <clei@secretmission.io>
2019-10-02 17:02:02 -0400

    add urls form

--- popup.js
+++ popup.js
@@ -1,12 +1,40 @@
-$("form").on("submit", function() {
-  value = $("input").val();
+chrome.storage.local.get(null, function(result) {
+  $("#display_params").append( `<p>${result.searchParams}</p>` );
 
-  chrome.storage.local.clear();
+  if (result.urls === undefined) {
+    return;
+  }
+
+  console.log(result.urls);
+
+  for (url of result.urls) {
+    $("#display_urls").append( `<p>${url}</p>` );
+  }
+});
+
+$("#search-form").on("submit", function() {
+  value = $("#search_params").val();
+
+  chrome.storage.local.remove("page");
   chrome.storage.local.set({"searchParams": value}, function() {
     console.log("Saved!")
   });
 });
 
-chrome.storage.local.get(null, function(result) {
-  $("#display_params").append( `<p>${result.searchParams}</p>` );
+$("#urls-form").on("submit", function() {
+  value = $("#urls").val();
+
+  chrome.storage.local.get(null, function(result) {
+    let test = [];
+
+    if (result.urls != undefined) {
+      test = result.urls
+    }
+
+    test.push(value)
+
+    chrome.storage.local.set({"urls": test}, function() {
+      console.log("Saved!")
+    });
+  });
 });
